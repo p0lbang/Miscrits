58e2e38ec1fc215a392732d143e884a7080638c5
Loxven <jeddgaustria@gmail.com>
2024-04-20 10:04:00 +0800

    more recursion fixes

--- miscrits.py
+++ miscrits.py
@@ -1,28 +1,56 @@
+import easyocr.imgproc
 import pyautogui
 import time
 import sys
+import easyocr
+
+import easyocr.character
 
 b = 0
 start = time.time()
 
+reader = easyocr.Reader(['en'])
+
 # SELECT WHICH ELEMENTS TO SEARCH IN ALTERNATION
-EXPLORE = ["eldertree.png", "eldershrub.png", "eldersunflower.png"]
+# EXPLORE = ["eldertree.png", "eldershrub.png", "eldersunflower.png", "elderleafpile.png"]
+# EXPLORE = ["a2_puddle.png", "a2_palm.png", "a2_stone.png", "a2_tree.png"]
+# EXPLORE = ["m0_cattail3.png", "m0_cattail2.png", "m0_cattail1.png", "m0_stone.png"]
+# EXPLORE = ["m2_sofa.png", "m2_statue.png", "m2_brush1.png", "m2_cage.png"]
+EXPLORE = ["m2_shelf.png", "m2_brush2.png", "m2_potions.png", "m2_woodcage.png"]
 WEAKNESS = "nature.png"
 
 
-def JEDD_locateCenterOnScreen(
-    imagename: str, confidence: float = 0.999
+def LXVI_locateCenterOnScreen(
+    imagename: str,
+    confidence: float = 0.999,
+    region: tuple[int, int, int, int] | None = None,
 ) -> pyautogui.Point | None:
     try:
-        return pyautogui.locateCenterOnScreen(imagename, confidence=confidence)
+        return pyautogui.locateCenterOnScreen(
+            imagename, confidence=confidence, region=region
+        )
     except pyautogui.ImageNotFoundException:
         return None
 
 
+def LXVI_readImage(
+    region: tuple[int, int, int, int] | None = None
+):
+    global reader
+
+    img = pyautogui.screenshot(region=region)
+    read = reader.readtext(img)
+    return read[0]
+
+
 def click(
-    imagename: str, confidence: float = 0.9, sleep: float = 0.2, duration: float = 0.2
+    imagename: str,
+    confidence: float = 0.9,
+    sleep: float = 0.2,
+    duration: float = 0.2,
+    region: tuple[int, int, int, int] | None = None,
 ) -> bool:
-    toClick = JEDD_locateCenterOnScreen(imagename, confidence=confidence)
+    toClick = LXVI_locateCenterOnScreen(imagename, confidence=confidence)
 
     if toClick is None:
         return False
@@ -35,11 +63,25 @@ def click(
 
 def searchMode():
     global EXPLORE
-    checkActive()
-    while JEDD_locateCenterOnScreen("expmultiplier.png", confidence=0.9) is not None:
+
+    if not checkActive():
+        print("Game not found during search mode, concluding process...")
+        conclude()
+
+    while LXVI_locateCenterOnScreen("expmultiplier.png", 0.85) is not None:
         SearchSuccess = False
+
+        if (
+            LXVI_locateCenterOnScreen(
+                "gold.png", confidence=0.8, region=[0, 100, 1920, 980]
+            )
+            is not None
+        ):
+            cleanUp()
+
         for search in EXPLORE:
-            if (toClick := JEDD_locateCenterOnScreen(search, confidence=0.9)) is None:
+            if (toClick := LXVI_locateCenterOnScreen(search, confidence=0.8)) is None:
+                time.sleep(1)
                 continue
 
             SearchSuccess = True
@@ -47,134 +89,139 @@ def searchMode():
             pyautogui.leftClick()
             time.sleep(4)
 
-            if JEDD_locateCenterOnScreen("expmultiplier.png", confidence=0.8) is None:
-                battleMode()
-                time.sleep(0.5)
+            if LXVI_locateCenterOnScreen("expmultiplier.png", 0.8) is None:
+                if LXVI_locateCenterOnScreen("battlebtns.png", 0.8) is not None:
+                    battleMode()
+                    summary()
 
         if not SearchSuccess:
-            checkActive()
             print("Elements not found, concluding process...")
             conclude()
 
-
-def battleMode():
-    global WEAKNESS, b
-    while True:
-        checkActive()
-
-        if JEDD_locateCenterOnScreen("miscripedia.png", confidence=0.8) is None:
-            b += 1
-            time.sleep(0.5)
-            summary()
-            return
-
-        time.sleep(0.1)
-
-        if (
-            toClick := JEDD_locateCenterOnScreen("run.png", confidence=0.99)
-        ) is not None:
-            pyautogui.moveTo(toClick, duration=0.2)
-
-            if JEDD_locateCenterOnScreen(WEAKNESS):
-                pyautogui.leftClick()
-            else:
-                pyautogui.moveRel(115, 80)
-                pyautogui.leftClick()
-                pyautogui.moveTo(toClick, duration=0.2)
-
-
-def summary():
-    checkActive()
-    try:
-        try:
-            pyautogui.locateOnScreen("trainable.png", confidence=0.6)
-            trainable = True
-        except Exception:
-            trainable = False
-
-        pyautogui.locateOnScreen("closebtn.png", confidence=0.9)
-        time.sleep(1)
-        pyautogui.moveTo(
-            pyautogui.locateOnScreen("closebtn.png", confidence=0.85), duration=0.1
-        )
-        pyautogui.leftClick()
-        time.sleep(1)
-
-        if trainable is True:
-            try:
-                toClick = pyautogui.locateCenterOnScreen("train.png", confidence=0.75)
-                pyautogui.moveTo(toClick, duration=0.2)
-                pyautogui.leftClick()
-                time.sleep(1)
-                train()
-            except Exception:
-                searchMode()
-        else:
-            searchMode()
-    except Exception:
-        time.sleep(1)
-        summary()
-
-
-def train():
-    checkActive()
-    try:
-        toClick = pyautogui.locateCenterOnScreen("trainable.png", confidence=0.6)
-    except Exception:
-        time.sleep(1)
-        click("x.png", 0.8)
-        searchMode()
     else:
-        try:
-            pyautogui.moveTo(toClick, duration=0.2)
-            pyautogui.leftClick()
-            time.sleep(0.2)
-            toClick = pyautogui.locateCenterOnScreen("train2.png", confidence=0.75)
-            pyautogui.moveTo(toClick, duration=0.2)
-            pyautogui.leftClick()
-            time.sleep(0.3)
-            toClick = pyautogui.locateCenterOnScreen("continuebtn.png", confidence=0.75)
-            pyautogui.moveTo(toClick, duration=0.2)
-            pyautogui.leftClick()
-            time.sleep(0.5)
-            try:
-                toClick = pyautogui.locateCenterOnScreen(
-                    "continuebtn2.png", confidence=0.75
-                )
-                pyautogui.moveTo(toClick, duration=0.2)
-                pyautogui.leftClick()
-                time.sleep(0.3)
-                toClick = pyautogui.locateCenterOnScreen(
-                    "continuebtn2.png", confidence=0.5
-                )
-                pyautogui.moveTo(toClick, duration=0.2)
-                pyautogui.leftClick()
-                time.sleep(0.3)
-                toClick = pyautogui.locateCenterOnScreen("skipbtn.png", confidence=0.75)
-                pyautogui.moveTo(toClick, duration=0.2)
-                pyautogui.leftClick()
-                time.sleep(0.3)
-            except Exception:
-                train()
-        except Exception:
-            train()
-
-
-def checkActive():
-    try:
-        pyautogui.locateOnScreen("appname.png", confidence=0.75)
-    except Exception:
-        print("Game is minimized, concluding process...")
-        time.sleep(0.5)
+        print("Minimized during search mode. Concluding process...")
         conclude()
 
 
+def cleanUp():
+    while (
+        LXVI_locateCenterOnScreen(
+            "gold.png", confidence=0.8, region=[0, 100, 1920, 980]
+        )
+        is not None
+    ):
+        click("gold.png", 0.9, region=[0, 100, 1920, 980])
+        continue
+    return
+
+
+def battleMode():
+    global WEAKNESS
+    r = 0
+    while True:
+        if not checkActive():
+            print("Minimized while in battle mode, concluding process...")
+            conclude()
+
+        if LXVI_locateCenterOnScreen("miscripedia.png", confidence=0.8) is None:
+            if LXVI_locateCenterOnScreen("closebtn.png", 0.85) is not None:
+                return
+
+        time.sleep(0.1)
+
+        if not LXVI_locateCenterOnScreen(WEAKNESS, confidence=0.8):
+            r = 1
+
+        if (
+            toClick := LXVI_locateCenterOnScreen("run.png", confidence=0.99)
+        ) is not None:
+            if r != 0:
+                # click("skillsetR.png", 0.75)
+
+                pyautogui.moveTo(toClick, duration=0.1)
+                pyautogui.moveRel(-45 + 160 * 1, 80)
+                pyautogui.leftClick()
+                pyautogui.moveTo(toClick, duration=0.1)
+
+                # click("skillsetL.png", 0.75)
+            else:
+                click("skillsetR.png", 0.75)
+
+                pyautogui.moveTo(toClick, duration=0.1)
+                pyautogui.moveRel(-45 + 160 * 1, 80)
+                pyautogui.leftClick()
+                r = 1
+
+                click("skillsetL.png", 0.75)
+
+                pyautogui.moveTo(toClick, duration=0.1)
+
+
+def summary():
+    global b
+    trainable = False
+
+    if not checkActive():
+        print("Minimized after battle, concluding process...")
+        conclude()
+    try:
+        time.sleep(1.5)
+        if LXVI_locateCenterOnScreen("trainable1.png", 0.675) is not None:
+            trainable = True
+
+        b += 1
+        miscrit = LXVI_readImage([1550, 80, 80, 20])
+        print(f"{b} Miscrits fought. Defeated: {miscrit}")
+        click("closebtn.png", 0.85, 1)
+
+        if trainable is True:
+            click("train.png", 0.75, 0.5)
+            train()
+            return
+
+    except Exception:
+        return
+
+
+def train():
+    while LXVI_locateCenterOnScreen("trainable.png", 0.6) is not None:
+        if not checkActive():
+            print("Minimized while training Miscrits, concluding process...")
+            conclude()
+
+        click("trainable.png", 0.6, 0.2, 0.2)
+        click("train2.png", 0.8, 0.5, 0.2)
+        click("continuebtn.png", 0.9, 1, 0.2)
+
+        click("continuebtn2.png", 0.75, 2, 0.2)
+        click("continuebtn3.png", 0.75, 2, 0.2)
+        click("skipbtn.png", 0.75, 2.5, 0.2)
+        continue
+
+    else:
+        click("x.png", 0.8)
+        return
+
+
+def checkActive():
+    if LXVI_locateCenterOnScreen("appname.png", 0.8, [0, 0, 1920, 100]) is not None:
+        return True
+    else:
+        return False
+
+
 def conclude():
-    print(f"Ended process after {b} Miscrits fought.")
+    print(f"Ended process after {b} Miscrit battles.")
     print(f"Runtime: {time.time()-start}")
-    sys.exit("Ending process.")
+    print()
+    time.sleep(1)
+    sys.exit()
 
 
-time.sleep(1)
-searchMode()
-sys.exit("Ended.")
+# time.sleep(2)
+print()
+if checkActive():
+    searchMode()
+else:
+    print("Game not found on screen. Nothing happened.")
+sys.exit()
